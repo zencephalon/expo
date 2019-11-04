79a8a95cd27c76ba139b86de3e5269f5c1f82580
Stanisław Chmiela <sjchmiela@gmail.com>
2019-11-04 12:03:18 +0100

    [expo] Do not log to terminal when Chrome remote debugger is enabled

--- packages/expo-yarn-workspaces/bin/AppEntry.template.js
+++ packages/expo-yarn-workspaces/bin/AppEntry.template.js
@@ -1,10 +1,19 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent, Logs } from 'expo';
 import { activateKeepAwake } from 'expo-keep-awake';
 
-import App from '{{relativeProjectPath}}/App';
+// https://github.com/expo/expo/issues/2623#issuecomment-441875640
+if (__DEV__) {
+  // https://github.com/expo/expo/issues/2623#issuecomment-441364587
+  const isRemoteDebuggingEnabled = typeof atob !== 'undefined';
+  if (isRemoteDebuggingEnabled) {
+    Logs.disableExpoCliLogging();
+  } else {
+    Logs.enableExpoCliLogging();
+  }
+}
 
 if (__DEV__) {
   activateKeepAwake();
 }
 
-registerRootComponent(App);
+registerRootComponent(require('{{relativeProjectPath}}/App').default);
