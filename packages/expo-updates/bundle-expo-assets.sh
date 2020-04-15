d4434438408f443172ec103491432ee760ee53b1
Eric Samelson <eric@expo.io>
2020-04-15 16:22:23 -0700

    fetch from packager server in createManifest script

--- packages/expo-updates/bundle-expo-assets.sh
+++ packages/expo-updates/bundle-expo-assets.sh
@@ -6,6 +6,6 @@ export NODE_BINARY=node
 ../node_modules/react-native/scripts/react-native-xcode.sh
 
 DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
-ASSETS=$(curl "http://localhost:8081/index.assets?platform=ios")
+ASSETS_URL="http://localhost:8081/index.assets?platform=ios"
 
-"$NODE_BINARY" ../node_modules/expo-updates/createManifest.js "$ASSETS" "$DEST"
+"$NODE_BINARY" ../node_modules/expo-updates/createManifest.js "$ASSETS_URL" "$DEST"
--- packages/expo-updates/createManifest.js
+++ packages/expo-updates/createManifest.js
@@ -1,36 +1,61 @@
 const fs = require('fs');
+const http = require('http');
 const path = require('path');
 
-const assetsJson = process.argv[2];
+const packagerUrl = process.argv[2];
 const destinationDir = process.argv[3];
 
-let assets;
-try {
-  assets = JSON.parse(assetsJson);
-} catch (e) {
-  throw new Error(
-    "Error parsing assets JSON from React Native packager. Ensure you've followed all expo-updates installation steps correctly. " +
-      e.message
-  );
-}
+(async function() {
+  const assetsJson = await new Promise(function(resolve, reject) {
+    http.get(packagerUrl, function(res) {
+      if (res.statusCode !== 200) {
+        reject(new Error('Request to packager server failed: ' + res.statusCode));
+        res.resume();
+        return;
+      }
 
-const manifest = {
-  commitTime: new Date().getTime(),
-  assets: [],
-};
-
-function addAsset(name, type, packagerHash) {
-  manifest.assets.push({ name, type, packagerHash });
-}
-
-assets.forEach(function(asset) {
-  asset.scales.forEach(function(scale, index) {
-    if (scale === 1) {
-      addAsset(asset.name, asset.type, asset.fileHashes[index]);
-    } else {
-      addAsset(asset.name + '@' + scale + 'x', asset.type, asset.fileHashes[index]);
-    }
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', function(chunk) {
+        rawData += chunk;
+      });
+      res.on('end', function() {
+        resolve(rawData);
+      });
+    });
   });
-});
 
-fs.writeFileSync(path.join(destinationDir, 'app.manifest'), JSON.stringify(manifest));
+  let assets;
+  try {
+    assets = JSON.parse(assetsJson);
+  } catch (e) {
+    throw new Error(
+      "Error parsing assets JSON from React Native packager. Ensure you've followed all expo-updates installation steps correctly. " +
+        e.message
+    );
+  }
+
+  const manifest = {
+    commitTime: new Date().getTime(),
+    assets: [],
+  };
+
+  function addAsset(name, type, packagerHash) {
+    manifest.assets.push({ name, type, packagerHash });
+  }
+
+  assets.forEach(function(asset) {
+    asset.scales.forEach(function(scale, index) {
+      if (scale === 1) {
+        addAsset(asset.name, asset.type, asset.fileHashes[index]);
+      } else {
+        addAsset(asset.name + '@' + scale + 'x', asset.type, asset.fileHashes[index]);
+      }
+    });
+  });
+
+  fs.writeFileSync(path.join(destinationDir, 'app.manifest'), JSON.stringify(manifest));
+})().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
