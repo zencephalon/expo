eaff90b46ad9be9610822285fcfaac8d7eedecc9
Cedric van Putten <me@bycedric.com>
2020-09-28 15:39:16 +0200

    [docs] Split next config into plugins

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,6 +2,7 @@ const { copySync, removeSync } = require('fs-extra');
 const merge = require('lodash/merge');
 const { join } = require('path');
 const semver = require('semver');
+const composePlugins = require('next-compose-plugins');
 
 const headings = require('./common/headingsMdPlugin');
 const { version } = require('./package.json');
@@ -18,41 +19,8 @@ const latest = join('pages', 'versions', 'latest/');
 removeSync(latest);
 copySync(vLatest, latest);
 
-module.exports = {
+module.exports = composePlugins([withMdx, withPrevalConstants], {
   trailingSlash: true,
-  // Rather than use `@zeit/next-mdx`, we replicate it
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  webpack: (config, options) => {
-    // Add preval support for `constants/*` only and move it to the `.next/preval` cache.
-    // It's to prevent over-usage and separate the cache to allow manually invalidation.
-    // See: https://github.com/kentcdodds/babel-plugin-preval/issues/19
-    config.module.rules.push({
-      test: /.jsx?$/,
-      include: [join(__dirname, 'constants')],
-      use: merge({}, options.defaultLoaders.babel, {
-        options: {
-          // Keep this path in sync with package.json and other scripts that clear the cache
-          cacheDirectory: '.next/preval',
-          plugins: ['preval'],
-        },
-      }),
-    });
-    // Add support for MDX with our custom loader
-    config.module.rules.push({
-      test: /.mdx?$/, // load both .md and .mdx files
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: { remarkPlugins: [headings] },
-        },
-        join(__dirname, './common/md-loader'),
-      ],
-    });
-    // Fix inline or browser MDX usage: https://mdxjs.com/getting-started/webpack#running-mdx-in-the-browser
-    config.node = { fs: 'empty' };
-    return config;
-  },
   // Create a map of all pages to export
   async exportPathMap(defaultPathMap, { dev, outDir }) {
     if (dev) {
@@ -93,4 +61,64 @@ module.exports = {
     });
     return pathMap;
   },
-};
+});
+
+/**
+ * Add MDX support to Next using our custom loader and plugins.
+ * The `./common/md-loader` is used to add frontmatter and the default wrapper.
+ * For the TOC we use a custom headings remark plugin.
+ */
+function withMdx(nextConfig = {}) {
+  return Object.assign({}, nextConfig, {
+    pageExtensions: [...nextConfig.pageExtensions, 'md', 'mdx'],
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /.mdx?$/,
+        use: [
+          options.defaultLoaders.babel,
+          {
+            loader: '@mdx-js/loader',
+            options: { remarkPlugins: [headings] },
+          },
+          join(__dirname, './common/md-loader'),
+        ],
+      });
+
+      // Fix inline or browser MDX usage
+      // See: https://mdxjs.com/getting-started/webpack#running-mdx-in-the-browser
+      config.node = { fs: 'empty' };
+
+      return typeof nextConfig.webpack === 'function'
+        ? nextConfig.webpack(config, options)
+        : config;
+    },
+  });
+}
+
+/**
+ * Add preval support for `constants/*` only and move it the output to `.next/preval` cache.
+ * It's to prevent over-usage and separate the cache to allow manually invalidation.
+ *
+ * @see https://github.com/kentcdodds/babel-plugin-preval/issues/19
+ */
+function withPrevalConstants(nextConfig = {}) {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /.jsx?$/,
+        include: [join(__dirname, 'constants')],
+        use: merge({}, options.defaultLoaders.babel, {
+          options: {
+            // Keep this path in sync with package.json and other scripts that clear the cache
+            cacheDirectory: '.next/preval',
+            plugins: ['preval'],
+          },
+        }),
+      });
+
+      return typeof nextConfig.webpack === 'function'
+        ? nextConfig.webpack(config, options)
+        : config;
+    },
+  });
+}
